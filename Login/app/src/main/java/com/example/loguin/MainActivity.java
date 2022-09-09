f6c9ddad610e129fe9f73a2776951e92b134a840
camilotenorio1234 <99371279+camilotenorio1234@users.noreply.github.com>
2022-09-09 17:40:28 -0500

    Login_simple

---
+++ Login/app/src/main/java/com/example/loguin/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.loguin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.LOGIN);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,Calculator.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
